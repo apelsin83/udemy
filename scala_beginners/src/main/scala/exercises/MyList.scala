ad24a9788fe624b17c1721d3d7ef1d80e2db477a
Vasili Litvin <vasili.litvin@stackpath.com>
2019-11-12 12:32:48 +0200

    Channges

--- scala_beginners/src/main/scala/exercises/MyList.scala
+++ scala_beginners/src/main/scala/exercises/MyList.scala
@@ -98,7 +98,7 @@ case class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
 
 object ListTest extends App {
 
-  val list = new Cons(1, new Cons(1, new Cons(3, Empty)))
+  val list = new Cons(1, Cons(1, Cons(3, Empty)))
 
   println(list.tail.head)
   println(list.add(4).head)
