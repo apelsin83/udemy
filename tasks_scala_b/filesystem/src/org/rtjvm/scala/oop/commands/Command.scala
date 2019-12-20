f930fcee5b2562943a61916a668bcc9dd08a5ede
Vasili Litvin <vasili.litvin@stackpath.com>
2019-12-20 11:37:37 +0200

    39 video

---
+++ tasks_scala_b/filesystem/src/org/rtjvm/scala/oop/commands/Command.scala
@@ -0,0 +1,12 @@
+package org.rtjvm.scala.oop.commands
+
+import org.rtjvm.scala.oop.filesystem.State
+
+trait Command {
+  def apply(state: State): State
+}
+
+object Command {
+
+  def from(input: String): Command = new UnknownCommand
+}
---
+++ tasks_scala_b/filesystem/src/org/rtjvm/scala/oop/filesystem/Filesystem.scala
@@ -0,0 +1,19 @@
+package org.rtjvm.scala.oop.filesystem
+
+import java.util.Scanner
+
+import org.rtjvm.scala.oop.commands.Command
+import org.rtjvm.scala.oop.files.Directory
+
+object FileSystem extends App {
+
+  val root = Directory.ROOT
+  var state = State(root, root)
+  val scanner = new Scanner(System.in)
+
+  while (true) {
+    state.show
+    val input = scanner.nextLine()
+    state = Command.from(input).apply(state)
+  }
+}
